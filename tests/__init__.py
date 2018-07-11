55fa34b0313c3a8035136edf8761a883dd1f876a
Alessandro Iob <alessandro.iob@gmail.com>
2018-07-11 14:51:12 +0200

    add unit tests

---
+++ tests/__init__.py
@@ -0,0 +1,14 @@
+import unittest
+import uuid
+
+import app
+
+class AppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.client = app.app.test_client()
+
+
+    def test_brigade_ci_meetup(self):
+        resp = self.client.get('/')
+        assert uuid.UUID(resp.data)
